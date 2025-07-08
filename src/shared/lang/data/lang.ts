c38c85d7e47a60bd38a75089380d3cf0b97a0d7b
Doni <donibro357@gmail.com>
2025-07-08 15:22:40 +0600

    add / languages, skeleton

---
+++ src/shared/lang/data/lang.ts
@@ -0,0 +1,9 @@
+const langArray: { code: string; label: string }[] = [
+  { code: "ru", label: "Русский" },
+  { code: "en", label: "English" },
+  { code: "kr", label: "한국인" },
+  { code: "cn", label: "中国人" },
+  { code: "jp", label: "日本語" },
+];
+
+export { langArray };
---
+++ src/widgets/skeleton/ui/Skeleton.tsx
@@ -0,0 +1,44 @@
+import { Skeleton, ConfigProvider } from "antd";
+
+const SkeletonComponent = () => {
+  return (
+    <ConfigProvider theme={{ components: { Skeleton: { color: "#1c1e29" } } }}>
+      <div className="flex flex-col gap-y-2">
+        <Skeleton.Image
+          active
+          className="rounded-[10px]"
+          style={{ width: 300, height: 200 }}
+        />
+        <Skeleton.Button
+          active
+          className="rounded-[10px]"
+          style={{ width: "100%", height: 20 }}
+        />
+        <Skeleton.Button
+          active
+          className="rounded-[10px]"
+          style={{ width: "30%", height: 13 }}
+        />
+        <div className="w-full flex justify-between">
+          <Skeleton.Avatar
+            active
+            className="rounded-full"
+            style={{ width: 30, height: 30 }}
+          />{" "}
+          <Skeleton.Avatar
+            active
+            className="rounded-full"
+            style={{ width: 30, height: 30 }}
+          />{" "}
+          <Skeleton.Avatar
+            active
+            className="rounded-full"
+            style={{ width: 30, height: 30 }}
+          />
+        </div>
+      </div>
+    </ConfigProvider>
+  );
+};
+
+export { SkeletonComponent };
