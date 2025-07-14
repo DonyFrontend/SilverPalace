193a2aee4a19b149c29e9408d7823a92a0a780db
iskak2007 <isaak9894@gmail.com>
2025-07-14 16:17:41 +0600

    add/aboutus

--- src/shared/config/router.config.tsx
+++ src/shared/config/router.config.tsx
@@ -1,8 +1,10 @@
+import { AboutUs } from "@/pages/AboutUs";
 import { Main } from "@/pages/main";
 import type React from "react";
 
 export enum RouterEnum {
   MAIN = "/",
+  ABOUTUS = '/AboutUs'
 }
 
 export interface RouterType {
@@ -14,5 +16,9 @@ export const RoutesObject: Record<RouterEnum, RouterType> = {
   [RouterEnum.MAIN]: {
     path: RouterEnum.MAIN,
     element: <Main />,
+  },
+  [RouterEnum.ABOUTUS]: {
+    path: RouterEnum.ABOUTUS,
+    element: <AboutUs />,
   },
 };
