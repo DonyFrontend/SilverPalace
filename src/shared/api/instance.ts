192e622449df8d6388c38a7d737bef67be82610d
iskak2007 <isaak9894@gmail.com>
2025-07-18 14:21:27 +0600

    merge

--- src/shared/api/instance.ts
+++ src/shared/api/instance.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const token = localStorage.getItem("spg_token");
 export const instance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     Authorization: "Bearer " + token,
     "ngrok-skip-browser-warning": "true",
